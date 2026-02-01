7ce9878480874122b152eb0f5a5a96af11a4936c
deeqasatech <deeqasatech@gmail.com>
2026-02-01 14:21:22 +0000

    Try fixing this error: `Console Error: Failed to fetch from https://gene

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({projectId: 'studio-3034637948-253c5'})],
   model: 'googleai/gemini-2.5-flash',
 });
